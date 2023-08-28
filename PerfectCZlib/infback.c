8295883fd760f601a2c8c3236af83c8c35f941c6
RockfordWei <rocky@perfect.org>
2023-08-28 08:36:57 -0400

    suppressing warnings

--- PerfectCZlib/infback.c
+++ PerfectCZlib/infback.c
@@ -1,3 +1,4 @@
+#pragma clang diagnostic ignored "-Wdeprecated-non-prototype"
 /* infback.c -- inflate using a call-back interface
  * Copyright (C) 1995-2016 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
--- PerfectCZlib/inflate.c
+++ PerfectCZlib/inflate.c
@@ -1,3 +1,4 @@
+#pragma clang diagnostic ignored "-Wdeprecated-non-prototype"
 /* inflate.c -- zlib decompression
  * Copyright (C) 1995-2016 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
